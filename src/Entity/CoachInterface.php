72dee3748833b1579620a7f073b3953977e51a88
stiff.roy <stiff.roy@einzigtech.com>
2023-02-06 00:26:32 +0100

    [TASK] Updating the coach interface

--- src/Entity/CoachInterface.php
+++ src/Entity/CoachInterface.php
@@ -5,4 +5,7 @@ namespace App\Entity;
 interface CoachInterface
 {
     public function getId();
+    public function getWeight();
+    public function getLength();
+    public function getMaxPassenger();
 }
